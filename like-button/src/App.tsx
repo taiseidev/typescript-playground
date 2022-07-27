f2a57283458918635fdbfcb7e69f1b5f37b677ab
onishi taisei <taisei.developer@gmail.com>
2022-07-27 22:20:08 +0900

    いいねボタンを配置するためLikeButtonタグを定義

--- like-button/src/App.tsx
+++ like-button/src/App.tsx
@@ -6,21 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LikeButton />
       </header>
     </div>
   );
+}
+
+function LikeButton() {
+  return (
+    <span>いいねボタン予定</span>
+  )
 }
 
 export default App;
