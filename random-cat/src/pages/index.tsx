5a71834e966a1ae99f7cf353495804f8e724d8e6
onishi taisei <taisei.developer@gmail.com>
2022-07-27 23:54:24 +0900

    ランダムで猫の画像を返すアプリを作成

--- random-cat/src/pages/index.tsx
+++ random-cat/src/pages/index.tsx
@@ -1,9 +1,36 @@
+// useStateを利用できるようにReactをimport
 import { useState } from "react";
 
+// 画像URLをリストで定義
+const catImages: string[] = [
+  "https://cdn2.thecatapi.com/images/bpc.jpg",
+  "https://cdn2.thecatapi.com/images/eac.jpg",
+  "https://cdn2.thecatapi.com/images/6qi.jpg",
+];
+
+// ランダムな画像を返す関数を定義
+const randomCatImage = (): string => {
+  const index = Math.floor(Math.random() * catImages.length);
+  return catImages[index];
+};
+
+// 画面を描画
 const IndexPage = () => {
   const [catImageUrl, setCatImageUrl] = useState(
     "https://cdn2.thecatapi.com/images/bpc.jpg"
   );
-  return <img src={catImageUrl} />;
+
+  const handleClick = () => {
+    setCatImageUrl(randomCatImage());
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>今日のにゃんこ🐱</button>
+      <div style={{ marginTop: 8 }}>
+        <img src={catImageUrl} />
+      </div>
+    </div>
+  );
 };
+
 export default IndexPage;
