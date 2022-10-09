889efba580f47971306df8010a6b5882889a68c0
onishi taisei <taisei.developer@gmail.com>
2022-10-09 22:08:41 +0900

    type-challengeのwarm-up解答

---
+++ src/challenge/warm-up.ts
@@ -0,0 +1,14 @@
+// Hello, World!
+
+// In Type Challenges, we use the type system itself to do the assertion.
+
+// For this challenge, you will need to change the following code to make the tests pass (no type check errors).
+
+// // expected to be string
+// type HelloWorld = any
+// // you should make this work
+// type test = Expect<Equal<HelloWorld, string>>
+// Click the Take the Challenge button to start coding! Happy Hacking!
+
+// answer
+type HelloWorld = string
