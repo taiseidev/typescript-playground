5b66a6a8e037f4c7b949d3ec450248b60e008552
onishi taisei <taisei.developer@gmail.com>
2022-10-09 15:47:08 +0900

    webpackの設定を修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,33 @@
+const path = require("path");
+
 module.exports = {
   // webpackでファイルを一つにまとめる際に起点となるファイル
   entry: {
     bundle: "./src/index.ts",
+  },
+  // バンドルしたファイルをどこに出力するか
+  // __dirnameはプロジェクトディレクトリのpathを取得
+  // distとjoinしてdistディレクトリ下にwebpackでバンドルしたファイルを出力
+  output: {
+    path: path.join(__dirname, "dist"),
+    // bundle:に紐づいたファイルを名を出力
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  // ローカルのwebサーバーが参照するディレクトリ先
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    // trueにするとwebサーバーを立ち上げた時に自動的にブラウザを開いてくれる
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        loader: "ts-loader",
+        test: /\.ts$/,
+      },
+    ],
   },
 };
