924c13f5b33e25f1773751d8ca58a9e070138e1f
onishi taisei <taisei.developer@gmail.com>
2022-10-13 21:52:33 +0900

    ジェネリック型を使わない関数を定義

---
+++ src/generics/basic.ts
@@ -0,0 +1,22 @@
+export default function genericsBasicSample() {
+  // ジェネリック型を使わない場合
+  const stringReduce = (array: string[], initialValue: string): string => {
+    let result = initialValue
+    for (let i = 0; i < array.length; i++) {
+      result += array[i]
+    }
+    return result
+  }
+
+  console.log('Generics basic sample 1:', stringReduce(['May ', 'I ', 'help ', 'you'], ''))
+
+  const numberReduce = (array: number[], initialValue: number): number => {
+    let result = initialValue
+    for (let i = 0; i < array.length; i++) {
+      result += array[i]
+    }
+    return result
+  }
+
+  console.log('Generics basic sample 2:', numberReduce([100, 200, 300], 1000))
+}
