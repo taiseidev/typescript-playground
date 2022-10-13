0cb7db97e7f2475780afb496401daaf678a5ef61
onishi taisei <taisei.developer@gmail.com>
2022-10-13 22:12:10 +0900

    色々なジェネリクス型の定義方法

--- src/generics/basic.ts
+++ src/generics/basic.ts
@@ -39,4 +39,15 @@ export default function genericsBasicSample() {
   }
 
   console.log('Generics basic sample 3:', genericsStringReduce(['Can ', 'I ', 'help ', 'you', '?'], ''))
+
+  // いろいろなジェネリック型の定義方法
+  // 完全な呼び出しシグネチャ（個々のシグネチャにジェネリック型を割り当てる）
+  type GenericReduce2 = {
+    <T>(array: T[], initialValue: T): T
+    <U>(array: U[], initialValue: U): U
+  }
+
+  // 呼び出しシグネチャの省略記法
+  type GenericReduce3<T> = (array: T[], initialValue: T) => T
+  type GenericReduce4 = <T>(array: T[], initialValue: T) => T
 }
