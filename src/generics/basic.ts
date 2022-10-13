cb67f4d43b62e70cabe28a0b27d3f9a8f8ac76de
onishi taisei <taisei.developer@gmail.com>
2022-10-13 22:01:53 +0900

    基本的なジェネリックの使い方

--- src/generics/basic.ts
+++ src/generics/basic.ts
@@ -19,4 +19,24 @@ export default function genericsBasicSample() {
   }
 
   console.log('Generics basic sample 2:', numberReduce([100, 200, 300], 1000))
+
+  type Reduce = {
+    (array: string[], initialValue: string): string
+    (array: number[], initialValue: number): number
+  }
+
+  // ジェネリックの型定義
+  type GenericReduce<T> = {
+    (array: T[], initialValue: T): T
+  }
+
+  const genericsStringReduce: GenericReduce<string> = (array, initialValue) => {
+    let result = initialValue
+    for (let i = 0; i < array.length; i++) {
+      result += array[i]
+    }
+    return result
+  }
+
+  console.log('Generics basic sample 3:', genericsStringReduce(['Can ', 'I ', 'help ', 'you', '?'], ''))
 }
