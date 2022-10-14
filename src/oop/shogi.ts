c7eaa2dfa1d5f9984705f9ce18e4446bf495ebbb
onishi taisei <taisei.developer@gmail.com>
2022-10-14 20:27:26 +0900

    オブジェクト指向について学習

---
+++ src/oop/shogi.ts
@@ -0,0 +1,58 @@
+type Suji = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+type Dan = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+type Player = 'first' | 'second'
+
+// 駒の位置を表すクラス
+class Position {
+  constructor(private suji: Suji, private dan: Dan) {}
+
+  // パラメーターに渡された位置と現在のの位置を比較するメソッド
+  distanceFrom(position: Position, player: Player) {
+    if (player === 'first') {
+      return {
+        suji: Math.abs(position.suji - this.suji),
+        dan: Math.abs(Number(position.dan) - Number(this.dan)),
+      }
+    } else {
+      return {
+        suji: Math.abs(position.suji - this.suji),
+        dan: -Math.abs(Number(position.dan) - Number(this.dan)), // 段（縦の位置）は正負を反転
+      }
+    }
+  }
+}
+
+// 駒を表すクラス
+abstract class Piece {
+  // Pieceクラスとサブクラスでアクセスできる
+  protected position: Position
+
+  constructor(private readonly player: Player, suji: Suji, dan: Dan) {
+    this.position = new Position(suji, dan)
+  }
+
+  // メソッドの定義
+  // 駒の移動用のメソッド
+  moveTo(position: Position) {
+    this.position = position
+  }
+
+  // 移動できるかどうかを判定するメソッド
+  abstract canMoveTo(position: Position, player: Player): boolean
+}
+
+class Osho extends Piece {
+  // 王将のcanMoveToメソッドを具体的に定義
+  canMoveTo(position: Position, player: Player): boolean {
+    const distance = this.position.distanceFrom(position, player)
+    // 移動範囲が2未満だったら移動できる（王将の移動範囲）
+    return distance.suji < 2 && distance.dan < 2
+  }
+}
+
+class Game {
+  private pieces = Game.makePieces()
+  private static makePieces() {
+    return [new Osho('first', 5, '1'), new Osho('second', 5, '9')]
+  }
+}
